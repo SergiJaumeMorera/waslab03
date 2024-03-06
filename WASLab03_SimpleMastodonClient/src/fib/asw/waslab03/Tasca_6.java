8b9ef7e1377ae122519af518cbea8e16c54efa8e
miquel <miquel.gibert.garcia@estudiantat.upc.edu>
2024-03-06 16:47:36 +0100

    tasca #6 print 10 tags

--- WASLab03_SimpleMastodonClient/src/fib/asw/waslab03/Tasca_6.java
+++ WASLab03_SimpleMastodonClient/src/fib/asw/waslab03/Tasca_6.java
@@ -1,10 +1,52 @@
 package fib.asw.waslab03;
 
+import org.apache.hc.client5.http.fluent.Request;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class Tasca_6 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Encara no faig res!");
+		String URIst = "https://mastodont.cat/api/v1/trends/tags";
+		String tokenst = Token.get();
+		String idAcc = "";
+
+		try {
+			String output = Request.get(URIst).addHeader("Authorization","Bearer "+tokenst).execute().returnContent().asString();
+	
+			JSONArray result = new JSONArray(output);
+			
+			for (int i = 0; i < 10; ++i) {
+				JSONObject resultObj = result.getJSONObject(i);
+				String nameT = resultObj.getString("name");
+				System.out.println(nameT);
+			}
+				
+		}
+		
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+/*
+		String URI = "https://mastodont.cat/api/v1/trends/tags" + nameTag;
+		String TOKEN = Token.get();
+
+		try {
+			String output = Request.get(URI)
+			.addHeader("Authorization","Bearer "+TOKEN)
+			.execute()
+			.returnContent()
+			.asString();
+	
+			JSONObject result = new JSONObject(output);
+			String text = result.getString("content");
+			System.out.format(text);
+		}
+		
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}*/
 	}
 
 }
